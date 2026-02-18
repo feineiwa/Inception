5ca9d50a708a9eeaaa3afcb8b2bf750c09aadf2b
Faneva herintsoa <rherintsoafaneva@gmail.com>
2026-02-18 09:22:18 +0300

    finins

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-services mariadb start
+set -e
 
-mysql -e "CREATE DATABASE IF NOT EXISTS faneva;"
-mysql -e "CREATE USER IF NOT EXISTS 'faneva'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON faneva.* TO 'faneva'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'faneva';"
-mysql -e "FLUSH PRIVILEGES;"
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 
-mysqladmin -u root -pfaneva shutdown
+mysqld_safe & 
+until mysqladmin ping --silent; do
+    sleep 1
+done
 
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
+
+mysqladmin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+# Start final server
 exec mysqld_safe
